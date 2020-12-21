b8cfcac349367f03df1c736f3bddebd07394b0e9
Agata Borkowska-Clark <borkowska.agata37@gmail.com>
2020-12-21 17:41:56 +0100

    day 21 p1 works for test, not actual input

--- src/day19.cpp
+++ src/day19.cpp
@@ -48,7 +48,41 @@ void parse_rule(std::string& line) {
 }
 
 /* Checks if the beginning of the str matches the given rule and updates start to point at the end of match */
-bool confirm_match(int rule_id, size_t& start, const std::string& str) {
+bool confirm_match(int rule_id, size_t& start, const std::string& str, int next_rule = 0) {
+  if (rule_id == 8 || rule_id == 11) {
+    std::cout << "looping rule " << rule_id << " with " << cfg[rule_id].subrules.size() << " subrules\n";
+   // keep applying 42 until it no longer matches
+    size_t i = start;
+    int count = 0;
+    bool match = true;
+    do {
+      match = confirm_match(42, i, str);
+      if (match) ++count;
+    } while (i < str.length() && match);
+    std::cout << "matched 42 " << count << " times\n";
+    if (i >= str.length()) {
+      if (rule_id == 8) return match;
+      std::cout << "i exceeded str length\n";
+      return false;
+    }
+    if (!match) return false;
+    if (rule_id == 8) {
+      std::cout << "updating start to " << i << '\n';
+      start = i;
+    } else {
+      // check that we have an appropriate number of 31s
+      while (count >= 0 && i < str.length() && match) {
+        match = confirm_match(31, i, str);
+      }
+      if (!match) {
+        return false;
+      }
+      if (count == 0 && match) {
+        start = i;
+      }
+    }
+    return match;
+  }
   if (cfg[rule_id].subrules.size() == 0) {
     if (str[start] == cfg[rule_id].terminal) {
       ++start;
@@ -61,8 +95,12 @@ bool confirm_match(int rule_id, size_t& start, const std::string& str) {
     for (const auto& v : r.subrules) {
       size_t i = start;
       bool sequence_match = true;
-      for (const auto& sr : v) {
-        sequence_match &= confirm_match(sr, i, str);
+      for (size_t sr = 0; sr < v.size(); ++sr) {
+        if (v[sr] == 8 && sr < v.size() - 1 && cfg[8].subrules.size() == 2) {
+          sequence_match &= confirm_match(v[sr], i, str, v[sr + 1]);
+        } else {
+          sequence_match &= confirm_match(v[sr], i, str);
+        }
         if (!sequence_match){
           break;
         }
@@ -81,7 +119,6 @@ int main() {
   while (std::getline(std::cin, line) && line.length() > 0) {
     parse_rule(line);
   }
-  // populate_parents();
   
   std::vector<std::string> input;
   int count = 0;
@@ -93,5 +130,24 @@ int main() {
     if (confirm_match(0, i, msg) && i == msg.length()) ++count;
   }
   std::cout << count << '\n';
+  std::vector<int> subrule1;
+  subrule1.push_back(42);
+  subrule1.push_back(8);
+  rule& r = cfg[8];
+  r.subrules.push_back(std::move(subrule1));
 
+  std::vector<int> subrule2;
+  subrule2.push_back(42);
+  subrule2.push_back(11);
+  subrule2.push_back(31);
+  rule& r2 = cfg[11];
+  r2.subrules.push_back(std::move(subrule2));
+  count = 0;
+  std::cout << "PART 2\n";
+  for (const auto& msg : input) {
+    std::cout << "parsing " << msg << '\n';
+    size_t i = 0;
+    if (confirm_match(0, i, msg) && i == msg.length()) ++count;
+  }
+  std::cout << count << '\n';
 }
---
+++ src/day21.cpp
@@ -0,0 +1,100 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <regex>
+#include <string>
+#include <vector>
+
+auto const word_regex = std::regex("[a-z]+");
+std::map<std::string, std::vector<std::vector<std::string>>> allergens_to_ingredients;
+std::map<std::string, std::vector<std::string>> allergens_to_ingredients_reduced;
+// list of all ingredients, from which the ones matched to allergens will be removed
+std::map<std::string, int> all_ingredients; 
+
+int main() {
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    std::vector<std::string> ingredients;
+    std::sregex_iterator i = std::sregex_iterator(line.begin(), line.end(), word_regex);
+    std::smatch m = *i;
+    // get a list of ingredients
+    while (m.str() != "contains") {
+      all_ingredients.emplace(m.str(), 0);
+      all_ingredients[m.str()] += 1;
+      ingredients.push_back(m.str());
+      ++i;
+      m = *i;
+    }
+    std::sort(ingredients.begin(), ingredients.end());
+    ++i;
+    // add it to the allergens<->ingredient lists map
+    while (i != std::sregex_iterator()) {
+      std::cout << (*i).str() << '\n';
+      std::string allergen = (*i).str();
+      if (allergens_to_ingredients.find(allergen) != allergens_to_ingredients.end()) {
+        allergens_to_ingredients[allergen].push_back(std::move(ingredients));
+      } else {
+        std::vector<std::vector<std::string>> tmp;
+        tmp.push_back(ingredients);
+        allergens_to_ingredients[allergen] = tmp;
+      }
+      ++i;
+    }
+  }
+  // find the intersection of all ingredients list for each allergen
+  for (const auto& [allergen, ingredients] : allergens_to_ingredients) {
+    std::cout << "considering allergen " << allergen << " with " << ingredients.size() << " ingredients lists\n";
+    if (ingredients.size() == 1) {
+      allergens_to_ingredients_reduced.emplace(allergen, ingredients[0]);
+      continue;
+    }
+    std::vector<std::string> tmp = ingredients[0];
+      std::cout << "1st list has";
+      for (const auto& str : tmp) std::cout << " " << str;
+      std::cout << '\n';
+    for (size_t i = 1; i < ingredients.size(); ++i) {
+      std::cout << "list has";
+      for (const auto& str : ingredients[i]) std::cout << " " << str;
+      std::cout << '\n';
+      std::vector<std::string> result(tmp.size() + ingredients[i].size());
+      std::vector<std::string>::iterator it;
+      it = std::set_intersection(tmp.begin(), tmp.end(), ingredients[i].begin(), ingredients[i].end(), result.begin());
+      result.resize(it - result.begin());
+      tmp = result;
+      std::cout << "intersection has";
+      for (const auto& str : tmp) std::cout << " " << str;
+      std::cout << '\n';
+    }
+    allergens_to_ingredients_reduced.emplace(allergen, tmp);
+  }
+  // if an allergen has only one possible match, remove it from the lists of possible matches for other allergens
+  bool something_changed = true;
+  while (something_changed) {
+    something_changed = false;
+    for (const auto& [allergen, ingredients] : allergens_to_ingredients_reduced) {
+      if (ingredients.size() == 1) {
+        for (auto& [other_allergen, other_ingredients] : allergens_to_ingredients_reduced) {
+          if (allergen != other_allergen) {
+            std::string ingredient = ingredients[0];
+            auto location = std::find(other_ingredients.begin(), other_ingredients.end(), ingredient);
+            if (location != other_ingredients.end()) {
+              something_changed = true;
+              other_ingredients.erase(location);
+            }
+          }
+        }
+      }
+    }
+  }
+  // remove potential matches from the set of all ingredients
+  for (const auto& [allergen, ingredients] : allergens_to_ingredients_reduced) {
+    for (const auto& ingredient : ingredients) {
+      all_ingredients.erase(ingredient);
+    }
+  }
+  int count = 0;
+  for (const auto& [ingredient, number] : all_ingredients) {
+    count += number;
+  }
+    std::cout << count << '\n';
+}
